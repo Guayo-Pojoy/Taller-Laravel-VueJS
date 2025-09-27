7f2677b6ecc9fefceb91c87abd3de7721cd7c1d7
Guayo-Pojoy <jpojoyy@miumg.edu.gt>
2025-09-27 11:34:48 -0600

    feat(auth): proteger rutas de usuarios con Sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,29 +4,31 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UsuarioController;
 use App\Http\Controllers\Api\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-// Rutas para el controlador de usuarios, asignando nombres personalizados
-
-Route::prefix('usuarios')->group(function () {
-    Route::get('/listUsers', [UsuarioController::class, 'index']);
-    Route::post('/addUser', [UsuarioController::class, 'store']);
-    Route::get('/getUser/{id}', [UsuarioController::class, 'show']);
-    Route::put('/updateUser/{id}', [UsuarioController::class, 'update']);
-    Route::delete('/deleteUser/{id}', [UsuarioController::class, 'destroy']);
-});
-
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Información del usuario autenticado
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Rutas CRUD protegidas de usuarios
+    Route::prefix('usuarios')->group(function () {
+        Route::get('/listUsers', [UsuarioController::class, 'index']);
+        Route::post('/addUser', [UsuarioController::class, 'store']);
+        Route::get('/getUser/{id}', [UsuarioController::class, 'show']);
+        Route::put('/updateUser/{id}', [UsuarioController::class, 'update']);
+        Route::delete('/deleteUser/{id}', [UsuarioController::class, 'destroy']);
+    });
+
+    // Logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
